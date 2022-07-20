1552039beb3c4eacd2156ae41bbf0dcbdb2631c2
AgustinAponte23 <aponteagustin1@gmail.com>
2022-07-19 21:42:12 -0300

    Primera entrega

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+// Cronometro que cuenta hasta 10
+
+
+
+for ( let cronometro = 0; cronometro <= 10 ; cronometro++ ) {
+
+    console.log(cronometro);
+    
+    while (cronometro === 10) {
+        console.log("El cronometro se detuvo")
+        break;
+    }
+
+}
+
+
+
