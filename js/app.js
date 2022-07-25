119d5198a7d8161024675b013fe8a95c168a8140
AgustinAponte23 <aponteagustin1@gmail.com>
2022-07-25 10:25:32 -0300

    Calculador de cuotas

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
+// Variables globales
+let monto = prompt('Ingrese su monto');
+let cantidad = prompt('¿Cuantas cuotas quiere?');
 
-// Cronometro que cuenta hasta 10
+// Calculo de valor de cuotas segun monto y cantidad
+function calcular() {
+    let resultado = monto / cantidad;
 
-
-
-for ( let cronometro = 0; cronometro <= 10 ; cronometro++ ) {
-
-    console.log(cronometro);
+    return resultado;
     
-    while (cronometro === 10) {
-        console.log("El cronometro se detuvo")
-        break;
+}
+document.write("Sus "+ cantidad + " cuotas a pagar" + " tienen un valor de " + " $" + calcular()); 
+
+/* Calculo de cuotas según cantidad del 2 al 3 para panorama del usuario
+
+for (let ingreso = monto ; ingreso ; ingreso / 2) {
+    
+    return ingreso;
+
+    break;
+
+    if (ingreso === undefined || ingreso === 0) {
+       console.log('Ingreso no valido')
+    } else{
+        document.write(' Ya puede ver las cuotas calculadas en la consola')
     }
 
-}
+    break;
+    
+}*/
+
+
+
+
+
+
+
 
 
 
