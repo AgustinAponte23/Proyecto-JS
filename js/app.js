e1f15b60430a38644eaadacad64d787bd2db1596
AgustinAponte23 <aponteagustin1@gmail.com>
2022-07-30 14:06:25 -0300

    Funcion verificar

--- js/app.js
+++ js/app.js
@@ -1,33 +1,19 @@
 // Variables globales
 let monto = prompt('Ingrese su monto');
 let cantidad = prompt('¿Cuantas cuotas quiere?');
+let operacion = monto / cantidad;
 
-// Calculo de valor de cuotas segun monto y cantidad
-function calcular() {
-    let resultado = monto / cantidad;
-
-    return resultado;
-    
-}
-document.write("Sus "+ cantidad + " cuotas a pagar" + " tienen un valor de " + " $" + calcular()); 
-
-/* Calculo de cuotas según cantidad del 2 al 3 para panorama del usuario
-
-for (let ingreso = monto ; ingreso ; ingreso / 2) {
-    
-    return ingreso;
-
-    break;
-
-    if (ingreso === undefined || ingreso === 0) {
-       console.log('Ingreso no valido')
-    } else{
-        document.write(' Ya puede ver las cuotas calculadas en la consola')
+function verificacion() {
+    if (monto && cantidad) { // Si monto y cantidad existen hacer el calculo sino error
+        let resultadoFinal = "Tus " + cantidad + " cuotas a pagar son de $" + operacion;
+        return resultadoFinal;
+    }else{
+        let err = 'No se pudo calcular';
+        return err;
     }
+}
 
-    break;
-    
-}*/
+console.log(verificacion());
 
 
 
