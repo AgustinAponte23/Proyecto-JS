e07586bb7baecd0461fd219dc010759377e09822
AgustinAponte23 <aponteagustin1@gmail.com>
2022-08-02 11:13:41 -0300

    funcion

--- js/app.js
+++ js/app.js
@@ -1,19 +1,37 @@
-// Variables globales
-let monto = prompt('Ingrese su monto');
-let cantidad = prompt('¿Cuantas cuotas quiere?');
-let operacion = monto / cantidad;
 
-function verificacion() {
-    if (monto && cantidad) { // Si monto y cantidad existen hacer el calculo sino error
-        let resultadoFinal = "Tus " + cantidad + " cuotas a pagar son de $" + operacion;
+function operacion() {
+    let monto = prompt('Ingrese su monto');
+    let cantidad = prompt('¿Cuantas cuotas quiere?');
+    let resultado = monto/cantidad;
+
+    parseInt(monto, cantidad);
+
+    if (monto <= 10000 && cantidad <= 10) { // Establecer un limite de monto y cantidad
+
+        resultadoFinal = "Sus " + cantidad + " cuotas a pagar tienen un valor de " + " $ " + resultado;
         return resultadoFinal;
-    }else{
-        let err = 'No se pudo calcular';
-        return err;
+
+    } else {
+        alert("No se puede calcular mas de diez cuotas");
     }
 }
+console.log(operacion());
 
-console.log(verificacion());
+
+let pregunta = confirm("¿Desea realizar otro calculo?");
+
+if (pregunta) {
+    console.log(operacion());
+} else {
+    alert("Gracias por usar este simulador");
+}
+
+
+
+
+
+
+
 
 
 
